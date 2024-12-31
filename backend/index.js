3957e67ff5cccba2f8fb153a819846f177b17eca
SakethMahadevan <fizzfreshers@gmail.com>
2024-12-31 13:09:25 +0530

    .env added, updated index.js,package-lock.json and package.json

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const {chats} = require ("./data/data");
 
 const app = express()
+dotenv.config();
 
 app.get("/",(req,res) => {
     res.send("API is running");
@@ -11,4 +13,11 @@ app.get('/api/chat', async (req,res) => {
     res.send(chats);
 })  
 
-app.listen(5000,console.log("Server started on PORT 5000"));
+app.get('/api/chat/:id',(req,res)=>{
+    //console.log(req.params.id);
+    const singleChat = chats.find((c)=>c._id==req.params.id);
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
